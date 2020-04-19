99d7fb389f964ddff633715aa59ca78829537c56
Oleksandr Artebiakin <artebiakin@MacBook-Pro.local>
2020-04-19 14:19:07 +0300

    added transparent status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:cinema_app/screens/HomeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(HelloWorldApp());
+void main() => runApp(CinemaApp());
 
-class HelloWorldApp extends StatelessWidget {
+class CinemaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, //or set color with: Color(0xFF0000FF)
+    ));
     return MaterialApp(
       theme: ThemeData(
         brightness: Brightness.light,
